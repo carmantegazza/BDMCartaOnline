da85215eed252ef0303a505316909a6770d06005
carmantegazza <car.mantegazza@gmail.com>
2022-07-28 22:54:17 -0300

    desafio comp1

--- cartaOnline.js
+++ cartaOnline.js
@@ -1,22 +1,38 @@
-let pesos = "$"
 let miOrden = "";
-let miTotal = "";
-
-let itemPedido = prompt("Ingresa el numero de flight elegido")
-while (itemPedido != "0") {
-    let itemAgrega = prompt("Queres agregar la degustacion de quesos de " + itemPedido + " ? Ingresa 1 para si, 0 para no")
-    switch (itemAgrega) {
-        case "1":
-            miOrden = miOrden + itemPedido + itemAgrega;
-            alert("Orden de flight " + itemPedido + "con degustacion de quesos");
-            break
-        case "2":
-            alert("Orden de flight " + itemPedido + "sin degustacion de quesos");
-            break
-    }
+let itemPedido = parseInt(prompt("Ingresá el numero de tabla que querés pedir"));
+while (itemPedido < 4) {
+    let itemAgregar = prompt("Querés agregar la degustación de vinos? 1 para si, 0 para no");
+    miOrden = itemPedido + itemAgregar;
     break
-    miOrden = itemPedido + itemAgrega;
 }
-
-let miNombre = prompt("Tu nombre?")
-alert("Gracias " + miNombre + "! Tu orden es " + miOrden + "\n El total es $ " + miTotal)
+let miNombre = prompt("Cual es tu nombre?")
+let miOrdenDetalle = "";
+switch (miOrden) {
+    case "10":
+        miOrdenDetalle = "Tabla #1 sin degustación de vinos";
+        miTotal = 1500
+        break
+    case "20":
+        miOrdenDetalle = "Tabla #2 sin degustación de vinos";
+        miTotal = 2200
+        break
+    case "30":
+        miOrdenDetalle = "Tabla #3 sin degustación de vinos";
+        miTotal = 2700
+        break
+    case "11":
+        miOrdenDetalle = "Tabla #1 con degustación de vinos";
+        miTotal = 2300
+        break
+    case "21":
+        miOrdenDetalle = "Tabla #2 con degustación de vinos";
+        miTotal = 3000
+        break
+    case "31":
+        miOrdenDetalle = "Tabla #3 con degustación de vinos";
+        miTotal = 3500
+        break
+    default:
+        break
+}
+alert("Gracias, " + miNombre + "\n Tu orden es " + miOrdenDetalle + "\n El total es $" + miTotal);
